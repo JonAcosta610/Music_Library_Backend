cff748c9c6fdef989ac6e4016bf1139df35fccaa
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-02-16 15:44:34 -0500

    created postman collection + created get HTTP command on postman + added serializers to views file

--- music_backend_database/views.py
+++ music_backend_database/views.py
@@ -1,7 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import MusicSerializer
+from .models import Music
 
 # Create your views here.
 @api_view(['GET'])
 def music_list(request):
-    return Response('ok')
+    music = Music.objects.all()
+    serializer = MusicSerializer(music, many=True)
+    return Response (serializer.data)
