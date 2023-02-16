9af3ca46be20f4d3f3af0dd1cff20ef46c22b359
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-02-16 17:22:16 -0500

    registered music model with admin site + completed post functionality for music

--- music_backend_database/views.py
+++ music_backend_database/views.py
@@ -2,10 +2,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import MusicSerializer
 from .models import Music
+from rest_framework import status
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_list(request):
-    music = Music.objects.all()
-    serializer = MusicSerializer(music, many=True)
-    return Response (serializer.data)
+    if request.method == 'GET':
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many=True)
+        return Response (serializer.data)
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.data, status=400)
