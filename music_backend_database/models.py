765dc0b241a281f8ffc0c26696396a014ae2001c
Jonathan Acosta <jonathanacosta@Jonathans-MBP.lan>
2023-02-16 14:03:13 -0500

    created music backend app + created music backend model for music app + registered music backend and rest framework in project settings installed apps section

---
+++ music_backend_database/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Music(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    album =  models.CharField(max_length=255)
+    releaste_date = models.DateField
+    genre = models.CharField(max_length=255)
